23919490a87851c67637bee542e46c7ffe1f764a
CanyonSchubert <canyonschubert72@gmail.com>
2021-07-08 02:43:32 -0700

    more work on build command
    returns more in the payload, just need to finish filling out the payload, add a reactioncollector, and swap between embedded responses

--- commands/build.js
+++ commands/build.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name : 'build',
-    alias  : ['runes', 'masteries', 'items', 'skillorder'],
+    aliases : ['runes', 'masteries', 'items', 'skillorder'],
     description : 'This command will return the user with essentially build information for the given champion.',
     args : true,
     argoptions : ['[champion] [role]'],
@@ -13,49 +13,83 @@ module.exports = {
     async execute(message, args) {
         const { devMode } = require('../appConfig.json');
 
-        const validRoles = ['top','jungle','mid','bot','support'];
+            // Valid entries for the mandatory role argument
+        const validRoles = ['top','jungle','middle','adc','support'];
 
+            // Sets and checks the role arg
         var championRole = args.pop();
         if (!validRoles.includes(championRole)) {
-            message.channel.send("Please enter a valid role after your champion name. These include top, jungle, mid, bot, and support.");
+            message.channel.send("Please enter a valid role after your champion name. These include top, jungle, middle, adc, and support.");
             return;
         }
 
+            // Gets, checks, and cleans the champion enters
         var champion = await ChampionAPI.getChampionByName(args);
         if (champion == "Champion not found.") {
 			message.channel.send("I can\'t find that champion, please try again.");
 			return;
 		}
-
         var championName = this.specialClausesCheck(champion.name);
 
-        var uggHttpHijackStr = 'https://u.gg/lol/champions/' + championName + '/build?role=' + championRole;
-        const browser = await puppeteer.launch({headless:false});
+            // Puppeteer scraping of u.gg
+        var uggHttpHijackStr = 'https://u.gg/lol/champions/' + championName + '/build?region=kr&role=' + championRole;
+        const browser = await puppeteer.launch( { headless : false } );
         const page = await browser.newPage();
         await page.goto(uggHttpHijackStr, {waitUntil:'domcontentloaded'});
-
         var payload = await page.evaluate(this.pageEvaluation);
-        console.log(payload);
+        if (devMode.isOn) console.log(payload);
+        browser.close();
 
-        //console.log((await page.content()).split('champion-image')[1].substring(0, 100));
-
-        //var embedList = [];
+            // Embed messages set up and return
         var nextEmbed = new Discord.MessageEmbed()
-            .setTitle(champion.name)
+            .setTitle(champion.name + ' - ' + championRole.charAt(0).toUpperCase() + championRole.substring(1, championRole.length))
+            .setColor('#2e8fc2')
+            .setAuthor('Information from U.GG', null, uggHttpHijackStr)
             .setThumbnail(payload.championThumbnail)
+            .addField('Win Rate', payload.championWinRate, true)
+            .addField('Pick Rate', payload.championPickRate, true)
+            .addField('Ban Rate', payload.championBanRate, true)
+            .addField('Total Matches', payload.championTotalMatches)
+            .setImage('https://static.u.gg/assets/ugg/logo/ugg-logo.svg')
+            .setTimestamp()
+            .setFooter('Nexus.gg')
         ;
         return message.channel.send(nextEmbed);
-        /*embedList.push(nextEmbed);
-
-        for (embed in embedList) {
-            message.channel.send(embed);
-        }
-        return;*/
     },
     pageEvaluation() {
         console.log('PUPPETEER ENTRY POINT');
         var payloadObject = {
-            championThumbnail : document.getElementsByClassName('champion-image')[0].src
+            championThumbnail : document.getElementsByClassName('champion-image')[0].src,
+            championWinRate : document.getElementsByClassName('content-section champion-ranking-stats')[0].getElementsByClassName('value')[0].textContent,
+            championPickRate : document.getElementsByClassName('content-section champion-ranking-stats')[0].getElementsByClassName('value')[2].textContent,
+            championBanRate : document.getElementsByClassName('content-section champion-ranking-stats')[0].getElementsByClassName('value')[3].textContent,
+            championTotalMatches : document.getElementsByClassName('content-section champion-ranking-stats')[0].getElementsByClassName('value')[4].textContent,
+            runeTreePrimary : document.getElementsByClassName('rune-tree_header')[0].getElementsByClassName('perk-style-title')[0].textContent,
+            runeTreePrimaryImage : document.getElementsByClassName('rune-tree_header')[0].getElementsByClassName('rune-image-container')[0].children[0].src,
+            /*runeTreePrimaryKeystone : ,
+            runeTreePrimaryKeystoneImage : ,
+            runeTreePrimaryRowOne : ,
+            runeTreePrimaryRowOneImage : ,
+            runeTreePrimaryRowTwo : ,
+            runeTreePrimaryRowTwoImage : ,
+            runeTreePrimaryRowThree : ,
+            runeTreePrimaryRowThreeImage : ,
+            runeTreeSecondary : document.getElementsByClassName('rune-tree_header')[0].getElementsByClassName('perk-style-title')[0].textContent,
+            runeTreeSecondaryImage : document.getElementsByClassName('rune-tree_header')[0].getElementsByClassName('rune-image-container')[0].children[0].src,
+            runeTreeSecondaryFirstChoice : ,
+            runeTreeSecondaryFirstChoiceImage : ,
+            runeTreeSecondarySecondChoice : ,
+            runeTreeSecondarySecondChoiceImage : ,
+            runeTreeShardOne : ,
+            runeTreeShardOneImage : ,
+            runeTreeShardTwo : ,
+            runeTreeShardTwoImage : ,
+            runeTreeShardThree : ,
+            runeTreeShardThreeImage : ,
+            summonerSpellOne : ,
+            summonerSpellOneImage : ,
+            summonerSpellTwo : ,
+            summonerSpellTwoImage : ,*/
         };
 
         return payloadObject;
